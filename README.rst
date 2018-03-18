364eaff963396a1982bc6cfef62b813722e4aad2
Jurriaan Bremer <jbr@cuckoo.sh>
2018-03-18 08:51:58 +0100

    initial project structure

---
+++ README.rst
@@ -0,0 +1,10 @@
+Cockroach
+=========
+
+Cockroach is your primitive & immortal swiss army knife for research purposes.
+The idea and most of the implementation is based on work by
+`Maciej Kotowicz <mak@lokalhost.pl>`_.
+Cockroach is designed to be used by `Cuckoo Sandbox`_ and as such is modular,
+easy to modify & update, and should remain backwards compatible.
+
+.. _`Cuckoo Sandbox`: https://cuckoosandbox.org/
---
+++ setup.py
@@ -0,0 +1,38 @@
+# Copyright (C) 2018 Jurriaan Bremer.
+# This file is part of Roach - https://github.com/jbremer/roach.
+# See the file 'docs/LICENSE.txt' for copying permission.
+
+from setuptools import setup
+
+setup(
+    name="roach",
+    version="0.0.1",
+    author="Jurriaan Bremer",
+    author_email="jbr@cuckoo.sh",
+    packages=[
+        "roach",
+    ],
+    entry_points={
+        "console_scripts": [
+            "roach = roach.main:main",
+        ],
+    },
+    url="https://github.com/jbremer/roach",
+    license="GPLv3",
+    description="Cockroach is your primitive & immortal swiss army knife",
+    include_package_data=True,
+    install_requires=[
+        "cryptography>=2.1",
+    ],
+    extras_require={
+        ":sys_platform == 'win32'": [
+            "capstone-windows==3.0.4",
+        ],
+        ":sys_platform == 'darwin'": [
+            "capstone==3.0.5rc2",
+        ],
+        ":sys_platform == 'linux2'": [
+            "capstone==3.0.5rc2",
+        ],
+    },
+)
