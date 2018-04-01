0b3d89801c82f3075bef112e49b7b7a1efa430e5
Jurriaan Bremer <jbr@cuckoo.sh>
2018-04-01 22:22:10 +0200

    version 0.1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="roach",
-    version="0.1.1",
+    version="0.1.2",
     author="Jurriaan Bremer",
     author_email="jbr@cuckoo.sh",
     packages=[
