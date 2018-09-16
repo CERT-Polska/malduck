309f33ebd9183c95e41bedb8e6571177c9f19f18
Jurriaan Bremer <jbr@hatching.io>
2018-09-16 17:24:55 +0200

    basic domain and url verification

--- roach/verify.py
+++ roach/verify.py
@@ -1,10 +1,42 @@
 # Copyright (C) 2018 Jurriaan Bremer.
+# Copyright (C) 2018 Hatching B.V.
 # This file is part of Roach - https://github.com/jbremer/roach.
 # See the file 'docs/LICENSE.txt' for copying permission.
 
 import re
 
+# https://stackoverflow.com/a/52082649
+DOMAIN_REGEX = (
+    "^(?=.{1,255}$)(?!-)[A-Za-z0-9\-]{1,63}(\.[A-Za-z0-9\-]{1,63})*\.?(?<!-)$"
+)
+
+# The regex as we use it in Cuckoo.
+URL_REGEX = (
+    # HTTP/HTTPS.
+    "(https?:\\/\\/)"
+    "((["
+    # IP address.
+    "(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\."
+    "(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\."
+    "(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\."
+    "(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])]|"
+    # Or domain name.
+    "[a-zA-Z0-9\\.-]+)"
+    # Optional port.
+    "(\\:\\d+)?"
+    # URI.
+    "(/[\\(\\)a-zA-Z0-9_:%?=/\\.-]*)?"
+)
+
 class Verify(object):
     @staticmethod
     def ascii(s):
         return bool(re.match("^[\x20-\x7f]*$", s, re.DOTALL))
+
+    @staticmethod
+    def domain(s):
+        return bool(re.match(DOMAIN_REGEX, s, re.DOTALL))
+
+    @staticmethod
+    def url(s):
+        return bool(re.match(URL_REGEX, s, re.DOTALL))
--- tests/test_verify.py
+++ tests/test_verify.py
@@ -1,4 +1,5 @@
 # Copyright (C) 2018 Jurriaan Bremer.
+# Copyright (C) 2018 Hatching B.V.
 # This file is part of Roach - https://github.com/jbremer/roach.
 # See the file 'docs/LICENSE.txt' for copying permission.
 
@@ -7,3 +8,14 @@ from roach import verify
 def test_ascii():
     assert verify.ascii("hello world") is True
     assert verify.ascii("foobar\x00") is False
+
+def test_domain():
+    assert verify.domain("apple.com") is True
+    assert verify.domain("http://google.com") is False
+    assert verify.domain(":\xb4\xa1") is False
+
+def test_url():
+    assert verify.url("apple.com") is False
+    assert verify.url("http://google.com") is True
+    assert verify.url("http://bing.com/") is True
+    assert verify.url(":\xb4\xa1") is False
