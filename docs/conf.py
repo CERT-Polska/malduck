032261c5eda2a2e05f2b1b01264997ae92757052
Pawel Srokosz <pawel.srokosz@cert.pl>
2019-08-21 17:49:18 +0200

    Version bump

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, CERT Polska'
 author = 'CERT Polska'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.1'
+release = '2.1.0'
 
 
 # -- General configuration ---------------------------------------------------
