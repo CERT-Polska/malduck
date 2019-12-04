1aec8cc5f43b561b9d42d9eb996482df2a3bea24
Pawel Srokosz <pawel.srokosz@cert.pl>
2019-12-04 15:38:12 +0100

    Stop processing procmem if there are no Yara matches

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, CERT Polska'
 author = 'CERT Polska'
 
 # The full version, including alpha/beta/rc tags
-release = '3.0.0'
+release = '3.0.2'
 
 
 # -- General configuration ---------------------------------------------------
