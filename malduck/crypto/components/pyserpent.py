efea310fe2e998e4ffb1e0f8309be745b4aba696
Pawel Srokosz <pawel.srokosz@cert.pl>
2020-06-19 17:17:58 +0200

    Removed C-string from pyserpent copyright comments

--- malduck/crypto/components/pyserpent.py
+++ malduck/crypto/components/pyserpent.py
@@ -11,20 +11,20 @@
 # (gladman@seven77.demon.co.uk) which is subject to the following license.
 # This Python implementation is not subject to any other license.
 #
-# * This is an independent implementation of the encryption algorithm:
-# *
-# * Serpent by Ross Anderson, Eli Biham and Lars Knudsen
-# *
-# * which is a candidate algorithm in the Advanced Encryption Standard
-# * programme of the US National Institute of Standards and Technology
-# *
-# * Copyright in this implementation is held by Dr B R Gladman but I
-# * hereby give permission for its free direct or derivative use subject
-# * to acknowledgment of its origin and compliance with any conditions
-# * that the originators of the algorithm place on its exploitation.
-# *
-# * Dr Brian Gladman (gladman@seven77.demon.co.uk) 14th January 1999
-# */
+# This is an independent implementation of the encryption algorithm:
+#
+# Serpent by Ross Anderson, Eli Biham and Lars Knudsen
+#
+# which is a candidate algorithm in the Advanced Encryption Standard
+# programme of the US National Institute of Standards and Technology
+#
+# Copyright in this implementation is held by Dr B R Gladman but I
+# hereby give permission for its free direct or derivative use subject
+# to acknowledgment of its origin and compliance with any conditions
+# that the originators of the algorithm place on its exploitation.
+#
+# Dr Brian Gladman (gladman@seven77.demon.co.uk) 14th January 1999
+#
 #
 # The above copyright notice must not be removed.
 #
