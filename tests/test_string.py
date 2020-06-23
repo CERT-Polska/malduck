67e99c39d6668188e2c7a4e8ff2c8762cc92b9cf
Paweł Srokosz <pawel.srokosz@cert.pl>
2020-06-23 15:53:32 +0200

    Add bigint.unpack and bigint.pack methods (#19)
    * Added new Bigint with pack and unpack methods
    
    * Added documentation for new feature
    
    * Added sized bigint
    
    * Black reformat
    
    * Used ljust and rjust

--- tests/test_string.py
+++ tests/test_string.py
@@ -110,10 +110,15 @@ def test_bin_reverse():
 
 
 def test_bigint():
-    with pytest.raises(ValueError):
-        bigint(b"ABCD", 40)
-    assert bigint(b"ABCDE", 40) == 0x4544434241
-    assert bigint(0x44434241, 40) == b"ABCD\x00"
+    assert bigint.unpack(b"ABCDE") == 0x4544434241
+    assert bigint.pack(0x44434241) == b"ABCD"
+    assert bigint.unpack_be(b"ABCDE") == 0x4142434445
+    assert bigint.pack_be(0x44434241) == b"DCBA"
+
+    assert bigint.unpack(b"ABCDE", 4) == 0x44434241
+    assert bigint.pack(0x44434241, 8) == b"ABCD\x00\x00\x00\x00"
+    assert bigint.unpack_be(b"ABCDE", 4) == 0x41424344
+    assert bigint.pack_be(0x41424344, 8) == b"\x00\x00\x00\x00ABCD"
 
 
 def test_pack():
