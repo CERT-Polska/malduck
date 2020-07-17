9f7f0223355ef9abcfd3608dbd7537a9293645e0
Pawel Srokosz <pawel.srokosz@cert.pl>
2020-07-17 16:23:57 +0200

    Bump version: 4.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, CERT Polska'
 author = 'CERT Polska'
 
 # The full version, including alpha/beta/rc tags
-release = '4.0.0'
+release = '4.0.1'
 
 
 # -- General configuration ---------------------------------------------------
