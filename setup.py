7b81b97d96a3245408ae96dc3a1060c777249f68
Pawel Srokosz <pawel.srokosz@cert.pl>
2020-08-20 12:59:24 +0200

    Bump version: 4.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name="malduck",
-    version="4.0.1",
+    version="4.0.2",
     description="Malduck is your ducky companion in malware analysis journeys",
     author="CERT Polska",
     author_email="info@cert.pl",
