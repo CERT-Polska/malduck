07570855d63ddc23d1035b6e252b8d0742868142
Paweł Srokosz <pawel.srokosz@cert.pl>
2023-10-11 16:11:48 +0200

    Include image=True binaries in load_binaries_from_memory (#108)

--- malduck/procmem/binmem.py
+++ malduck/procmem/binmem.py
@@ -71,13 +71,22 @@ class ProcessMemoryBinary(ProcessMemory, metaclass=ABCMeta):
         """
         Looks for binaries in ProcessMemory object and yields specialized ProcessMemoryBinary objects
         :param procmem: ProcessMemory object to search
+
+        .. versionchanged:: 4.4.0
+
+            In addition to image=False binaries, it also returns image=True versions.
+            In previous versions it was done by extractor, so it was working only
+            if memory-aligned version was also "valid".
         """
         if cls.__magic__ is None:
             raise NotImplementedError()
         for binary_va in procmem.findv(cls.__magic__):
-            binary_procmem = cls.from_memory(procmem, base=binary_va)
-            if binary_procmem.is_valid():
-                yield binary_procmem
+            binary_procmem_dmp = cls.from_memory(procmem, base=binary_va)
+            if binary_procmem_dmp.is_valid():
+                yield binary_procmem_dmp
+            binary_procmem_img = binary_procmem_dmp.image
+            if binary_procmem_img and binary_procmem_img.is_valid():
+                yield binary_procmem_img
 
     @abstractmethod
     def is_image_loaded_as_memdump(self) -> bool:
