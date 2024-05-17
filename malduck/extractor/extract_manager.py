866192091318fa262ceaf7c0bb7e0116e064bc72
psrok1 <pawel.srokosz@cert.pl>
2024-05-17 12:07:47 +0200

    Fix debug message

--- malduck/extractor/extract_manager.py
+++ malduck/extractor/extract_manager.py
@@ -124,7 +124,7 @@ class ExtractManager:
             for carved_bin in carved_bins:
                 log.debug(
                     f"carve: Found {carved_bin.__class__.__name__} "
-                    f"at {carved_bin.imgbase}"
+                    f"at {hex(carved_bin.imgbase)}"
                 )
                 yield carved_bin
 
